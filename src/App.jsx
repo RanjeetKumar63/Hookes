fe819194911d3e8df3b0a7bd041b7a65fc9805cc
Ranjeet Kumar <143226519+RanjeetKumar63@users.noreply.github.com>
2025-01-17 10:21:51 +0530

    update the useEff

--- src/App.jsx
+++ src/App.jsx
@@ -27,43 +27,43 @@
 
 // export default App;
 
-import React, { useState } from "react";
-import "./App.css";
-
-const App = () => {
-  const [count, setCount] = useState(0);
-  const increaseCount = () => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
-  return (
-    <div>
-      <h1>Count : {count}</h1>
-      <button onClick={increaseCount}>Increase by 4</button>
-    </div>
-  );
-};
-
-export default App;
-
-// import { useEffect, useState } from "react";
-
+// import React, { useState } from "react";
 // import "./App.css";
 
 // const App = () => {
 //   const [count, setCount] = useState(0);
-//   useEffect(() => {
-//     setTimeout(() => {
-//       setCount((count) => count + 1);
-//     }, 2000);
-//   });
+//   const increaseCount = () => {
+//     setCount((count) => count + 1);
+//     setCount((count) => count + 1);
+//     setCount((count) => count + 1);
+//     setCount((count) => count + 1);
+//   };
 //   return (
 //     <div>
-//       <h1>I've rendered {count} times</h1>
+//       <h1>Count : {count}</h1>
+//       <button onClick={increaseCount}>Increase by 4</button>
 //     </div>
 //   );
 // };
 
 // export default App;
+
+// import { useEffect, useState } from "react";
+
+import "./App.css";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 2000);
+  });
+  return (
+    <div>
+      <h1>I've rendered {count} times</h1>
+    </div>
+  );
+};
+
+export default App;
