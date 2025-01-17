3a4b08f0f31adfe040b7d1651542f37c9d9878b8
Ranjeet Kumar <143226519+RanjeetKumar63@users.noreply.github.com>
2025-01-17 10:20:24 +0530

    update useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,52 @@
-import React, { useState } from "react";
-import "./App.css";
-
-const App = () => {
-  const [car, setCar] = useState({
-    brand: "Ferrari",
-    model: "Roma",
-    year: "2023",
-    color: "Red",
-  });
-  const changeColor = () => {
-    setCar((prev) => {
-      return { ...prev, color: "blue" };
-    });
-  };
-
-  return (
-    <div>
-      <h1>My {car.brand}</h1>
-      <h2>
-        It is a {car.color}, {car.model} & {car.year} Roma from 2023
-      </h2>
-      <button onClick={changeColor}>Blue</button>
-    </div>
-  );
-};
-
-export default App;
-
 // import React, { useState } from "react";
 // import "./App.css";
 
 // const App = () => {
-//   const [count, setCount] = useState(0);
-//   const increaseCount = () => {
-//     setCount((count) => count + 1);
-//     setCount((count) => count + 1);
-//     setCount((count) => count + 1);
-//     setCount((count) => count + 1);
+//   const [car, setCar] = useState({
+//     brand: "Ferrari",
+//     model: "Roma",
+//     year: "2023",
+//     color: "Red",
+//   });
+//   const changeColor = () => {
+//     setCar((prev) => {
+//       return { ...prev, color: "blue" };
+//     });
 //   };
+
 //   return (
 //     <div>
-//       <h1>Count : {count}</h1>
-//       <button onClick={increaseCount}>Increase by 4</button>
+//       <h1>My {car.brand}</h1>
+//       <h2>
+//         It is a {car.color}, {car.model} & {car.year} Roma from 2023
+//       </h2>
+//       <button onClick={changeColor}>Blue</button>
 //     </div>
 //   );
 // };
 
 // export default App;
+
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const increaseCount = () => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={increaseCount}>Increase by 4</button>
+    </div>
+  );
+};
+
+export default App;
 
 // import { useEffect, useState } from "react";
 
